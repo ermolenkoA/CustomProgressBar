2b9ba93f026206c940eb4477d326a85c2a16a974
ermolenkoA <kypalo2012@gmail.com>
2023-08-24 18:45:01 +0300

    Created project, added .gitignore, prepared main screen

---
+++ app/src/main/java/com/example/customprogressbar/Views/ProgressBarCustomView.kt
@@ -0,0 +1,9 @@
+package com.example.customprogressbar.Views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ProgressBarCustomView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+}
